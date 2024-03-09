76bb015a82dc249b4b05296499a9f882bad44e9e
kumar8317 <ankitkumar8317@gmail.com>
2024-03-09 18:20:38 +0530

    linting

--- src/commands/create-project/index.ts
+++ src/commands/create-project/index.ts
@@ -1,7 +1,7 @@
 import {Args, Command, Flags} from '@oclif/core'
 import chalk from 'chalk'
-import * as fs from "fs";
-import { execSync } from 'child_process';
+import { execSync } from 'node:child_process';
+import * as fs from "node:fs";
 
 export const starterTemplateRepo = 'https://github.com/kumar8317/forge-node-starter';
 export const SUCCESS_EXIT_CODE = 0;
@@ -9,7 +9,7 @@ export const OPTION_RECURSIVE = '-r';
 export const UTF8 = 'utf8';
 // Place release tag here
 export const releaseTag = 'v0.0.1';
-const Log = console.log;
+const { log }= console;
 
 function formatFeatureName(name: string) {
   return name.split(" ").join("-").toLocaleLowerCase();
@@ -21,8 +21,9 @@ function isDirectoryExist(path: string) {
 
 export default class CreateProject extends Command {
   static args = {
-    project_name: Args.string({description: 'Project Name', required: true}),
+    projectName: Args.string({description: 'Project Name', required: true}),
   }
+
   static description = 'Scaffolds a fresh Forge express project'
 
   static examples = ['forge create-project myNewProject']
@@ -32,21 +33,21 @@ export default class CreateProject extends Command {
   }
 
   async run(): Promise<void> {
-    const { args, flags} = await this.parse(CreateProject)
-    const projectName = formatFeatureName(args.project_name);
+    const { args } = await this.parse(CreateProject)
+    const projectName = formatFeatureName(args.projectName);
 
     if (isDirectoryExist(projectName)) {
       throw new Error(`There's already a Project with the name '${projectName}'`);
     }
 
-    Log(chalk.green(`Generating Project - ${chalk.yellow(projectName)} ....`));
+    log(chalk.green(`Generating Project - ${chalk.yellow(projectName)} ....`));
 
     const cloneCommand = `git clone ${starterTemplateRepo} --depth 1 --branch ${releaseTag} ${projectName} > /dev/null 2>&1`;
     try {
       execSync(cloneCommand,{stdio: 'inherit'})
       execSync(`npx rimraf ${projectName}/.git`,{stdio: 'inherit'})
-      Log(chalk.green('Project generated successfully'));
-      Log(chalk.green('-------------------------------'));
+      log(chalk.green('Project generated successfully'));
+      log(chalk.green('-------------------------------'));
     } catch (error) {
       console.error('Error during project generation:', error);
       throw new Error('Failed to create project. Please ensure you have an internet connection.');
